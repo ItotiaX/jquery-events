84109eba857c4b2f7187e93a374461088086ff4d
ItotiaX <tkmckinney12@gmail.com>
2025-04-27 21:06:57 -0700

    added events and signup links. Added new div - newsletter w/signup link. Added img of rose.
    In JS added slider for signup link
    Added fade and hide submission event
    
    added mouseover event to change slogan text
    
    added rose event to animate rose on started

--- script.js
+++ script.js
@@ -23,11 +23,82 @@ $(document).ready(function() {
         $(this).children('.botanic').show(); 
     });
 
-//keypress event
+//keypress events
     $(document).on('keypress', function (e) {
         let keyPressed = String.fromCharCode(e.which).toLowerCase();
         window.location = "#" + keyPressed;
     });
 });
 
+//hide signup info on load in
+$(document).ready(function() {
+	// Hide form
+	$('#newsSignup').hide();
+
+	// Signup click event
+	$('#signuplink').click(function(e) {
+		e.preventDefault(); // halt default behavior
+
+		// Toggle the form visibility
+		$('#newsSignup').slideToggle();
+
+		// Change the + to - 
+		var icon = $('#openclose');
+		if (icon.text() === '+') {
+			icon.text('-');
+		} else {
+			icon.text('+');
+		}
+	});
+});
+
+
+//slogan event
+$(document).ready(function() {
+  // Mouseover effect
+  $("#slogan").mouseover(function() {
+    // Fade out 
+    $(this).fadeOut("normal", "linear", function() {
+      // Callback function
+      $(this).text("Hand Picked Just for You").fadeIn("slow", "swing");
+    });
+  });
+  
+  // Mouseout effect
+  $("#slogan").mouseout(function() {
+    // Fade out 
+    $(this).fadeOut("fast", "swing", function() {
+      $(this).text("The Power of Flowers").fadeIn("slow", "linear");
+    });
+  });
+});
+
+
+//rose event
+$(document).ready(function(){
+  $('#rose').animate({
+    right: '100px',
+    opacity: 1
+  }, {
+    duration: 2000,
+    easing: 'swing'
+  });
+  
+
+});
+
+//signup event
+
+$(document).ready(function(){
+  $('#newsSignup').submit(function(e){
+    e.preventDefault(); // stops form
+    
+    alert('thank you for registering');
+    $('#newsSignup').hide();
+    $('#newsSignup').fadeTo('slow', 0.3);
+    
+  });
+
+
+});
 
